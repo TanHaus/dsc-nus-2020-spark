cee61f3c25f6cf313d103febb5c8b7f48d90ce8c
Dan Tran <picasdan99@gmail.com>
2020-03-09 11:54:41 +0800

    set up server attempt

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require("express");
-var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
