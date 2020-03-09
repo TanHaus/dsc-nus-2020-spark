da8169a23968a03b4b8b62d3f8eecae50eb6846f
Dan Tran <picasdan99@gmail.com>
2020-03-09 15:44:43 +0800

    test push request

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+
 app.get('/', (req, res) => res.send('CHROMATICA!'))
+
+app.post("/", (req, res) => {
+    res.send(`${req.body.test}`);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
