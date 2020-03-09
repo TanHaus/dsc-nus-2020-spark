a6fe8adea9d2652520431bf09085605ef442d711
Thien Tran <gau.nernst@yahoo.com.sg>
2020-03-10 00:50:21 +0800

    add html

--- server/server.js
+++ server/server.js
@@ -2,13 +2,44 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+class Lamp {
+    constructor(name) {
+        this.state = 0
+        this.name = name
+    }
+    toggleState() {
+        if(this.state === 1) this.state = 0
+        else this.state = 1
+    }
+}
+
+let names = ['lamp1', 'lamp2']
+let lamps = names.map(name => new Lamp(name))
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 
-app.get('/', (req, res) => res.send('CHROMATICA!'))
+app.get('/', (req, res) => {
+    res.sendFile('html/index.html', {root: __dirname})
+})
 
 app.post("/", (req, res) => {
     res.send(`I got you, ${req.body.test}`);
 });
+
+app.get('/state', (req,res) => {
+    let name = req.body.name
+    let index = names.indexOf(name)
+    if(index == -1) {
+        // do something
+    } else {
+        // do something
+    }
+    res.send()
+})
+
+app.post('/send', (req, res) => {
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
